7e72f48d92f9e145a521cdd6b0e4d0a1ba7eb9d5
Faraz <faraz.com>
2023-09-28 16:58:59 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import UserRouter from "./routes/user.route.js";
-
+import AuthRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -16,7 +16,9 @@ mongoose
     }
   });
 const app = express();
+app.use(express.json());
 app.use("/api/user", UserRouter);
+app.use("/api/auth", AuthRouter);
 
 app.listen(3000, () => {
   console.log("server is running at port-3000");
